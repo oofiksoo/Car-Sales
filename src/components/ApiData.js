a0cc8344a11baad8e2f78ea2db5b69c4a7e6ff9e
Joseph Graham <Joseph@Chasingthefiks.net>
2019-12-12 10:38:08 -0700

    MVP Met - api implemented - continuious loop - continueing to identify solution

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -2,25 +2,8 @@ import React, { useState } from "react";
 import CarCard from "./CarCard";
 
 export default function ApiData() {
-  const [listingdata, setListingData] = useState([]);
-  var proxyUrl = "https://cors-anywhere.herokuapp.com/",
-    targetUrl =
-      "https://marketcheck-prod.apigee.net/v1/search?api_key=QvZEqHZSO7OuNEiTA2PEaB9SCic6MrVo&seller_type=dealer&year=2019&make=ford&model=mustang";
-  fetch(proxyUrl + targetUrl)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setListingData(data.listings);
-      console.log(listingdata);
-    })
-    .catch(e => {
-      console.log(e);
-      return e;
-    });
+  
   return (
-    <div className="listings">
-      <h3>Other Vechicle Listings</h3>
-      <CarCard cars={listingdata} />
-    </div>
+    
   );
 }
